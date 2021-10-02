a8bd5994fd8ce9663ff296173c2bf75bd2cb80a3
diselmxx <diselmxx@gmail.com>
2021-10-02 21:29:13 +0300

    Личный проект: покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -6,80 +6,49 @@ const { getRandomInt, shuffle, getRandomDate } = require(`./utils`);
 const { ExitCode } = require(`../../constants`);
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
+const FILE_ANNOUNCES_PATH = `../../data/announces.txt`;
+const FILE_TITLES_PATH = `../../data/titles.txt`;
+const FILE_CATEGORIES_PATH = `../../data/categories.txt`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Ёлки. История деревьев`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
-const ANNOUNCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
-
-const randomSentencesNumber = getRandomInt(1, ANNOUNCES.length - 1);
-
-const generatePublication = (count) =>
-  Array(count)
+const generatePublication = (count, titles, categories, announces) => {
+  const randomSentencesNumber = getRandomInt(1, announces.length - 1);
+  return Array(count)
     .fill({})
     .map(() => ({
-      title: TITLES[getRandomInt(0, TITLES.length - 1)],
+      title: titles[getRandomInt(0, titles.length - 1)],
       createdDate: getRandomDate(),
-      announce: shuffle(ANNOUNCES).slice(1, 5).join(` `),
-      fullText: shuffle(ANNOUNCES).slice(1, randomSentencesNumber).join(` `),
-      category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+      announce: shuffle(announces).slice(1, 5).join(` `),
+      fullText: shuffle(announces).slice(1, randomSentencesNumber).join(` `),
+      category: [categories[getRandomInt(0, categories.length - 1)]],
     }));
+};
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const announces = await readContent(FILE_ANNOUNCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
     const [count] = args;
     if (count > 1000) {
       console.error(chalk.red(`Не больше 1000 объявлений`));
       process.exit(ExitCode.ERROR);
     }
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generatePublication(countOffer));
+    const content = JSON.stringify(
+      generatePublication(countOffer, titles, categories, announces)
+    );
+
     try {
       await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(`Operation success. File created.`));
