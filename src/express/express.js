39311ee68675a2860e9ad177cd30f6d4a297e324
diselmxx <diselmxx@gmail.com>
2021-10-13 21:32:20 +0300

    Личный проект: по тропе Аппалачей

--- src/express/express.js
+++ src/express/express.js
@@ -1,15 +1,12 @@
 "use strict";
 
 const express = require(`express`);
-const articlesRoutes = require(`./routes/articles-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const mainRoutes = require(`./routes/main-routes`);
+const router = require(`./routes/router`);
+
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
-app.use(`/`, mainRoutes);
-app.use(`/articles`, articlesRoutes);
-app.use(`/my`, myRoutes);
+app.use(router);
 
 app.listen(DEFAULT_PORT);
--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,17 +3,15 @@
 const { Router } = require(`express`);
 const articlesRouter = new Router();
 
-const getRouteId = (id) => Number.parseInt(id, 10);
-
 articlesRouter.get(`/category/:id`, (req, res) => {
-  res.send(`/articles/category/${getRouteId(req.params.id)}`);
+  res.send(`/articles/category/:id`);
 });
 articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
 articlesRouter.get(`/edit/:id`, (req, res) => {
-  res.send(`/articles/edit/${getRouteId(req.params.id)}`);
+  res.send(`/articles/edit/:id`);
 });
 articlesRouter.get(`/:id`, (req, res) => {
-  res.send(`/articles/${getRouteId(req.params.id)}`);
+  res.send(`/articles/:id`);
 });
 
 module.exports = articlesRouter;
