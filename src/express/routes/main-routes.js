e1a77fa9b8475a7dbced3beec4c7decdf5a44277
diselmxx <diselmxxx@gmail.com>
2021-12-25 17:38:29 +0300

    Личный проект:  обновить routes

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -2,13 +2,34 @@
 
 const {Router} = require(`express`);
 const mainRouter = new Router();
+const {getAPI} = require(`../api`);
+const api = getAPI();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  const [articles, categories] = await Promise.all([
+    api.getArticles(),
+    api.getCategories(),
+  ]);
+  res.render(`main`, {articles, categories});
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) =>
-  res.render(`search`, {wrapperClass: `wrapper-color`})
-);
+
+mainRouter.get(`/search`, async (req, res) => {
+  if (req.query.constructor === Object && Object.keys(req.query).length === 0) {
+    res.render(`search`, {wrapperClass: `wrapper-color`});
+  } else {
+    const results = await api.search(req.query);
+    res.render(`search`, {
+      results,
+      wrapperClass: `wrapper-color`,
+      searchResultsEmpty: results.length === 0,
+      query: req.query.query
+    });
+  }
+});
+
 mainRouter.get(`/categories`, (req, res) =>
   res.render(`all-categories`, {
     wrapperClass: `wrapper wrapper--nobackground`,
---
+++ src/express/utils.js
@@ -0,0 +1,8 @@
+
+"use strict";
+
+const ensureArray = (value) => (Array.isArray(value) ? value : [value]);
+
+module.exports = {
+  ensureArray,
+};
