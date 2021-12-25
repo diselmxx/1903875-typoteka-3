fa3897f2bc2bfc6874e1a67cce46da63023bfbbe
diselmxx <diselmxxx@gmail.com>
2021-12-25 17:46:16 +0300

    Личный проект:  обновить routes

--- src/express/express.js
+++ src/express/express.js
@@ -4,12 +4,17 @@ const express = require(`express`);
 const router = require(`./routes/router`);
 const path = require(`path`);
 const PUBLIC_DIR = `public`;
+const UPLOAD_DIR = `upload`;
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use(express.urlencoded({extended: false}));
 app.use(router);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
 
 app.set(`views`, path.resolve(__dirname, `templates/layouts`));
 app.set(`view engine`, `pug`);
