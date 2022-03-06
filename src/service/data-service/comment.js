6f85964f45a72a0dffb592bc7d218b278070732e
diselmxx <diselmxxx@gmail.com>
2022-03-06 15:20:05 +0300

    обновить сервисы

--- src/service/data-service/comment.js
+++ src/service/data-service/comment.js
@@ -1,37 +1,34 @@
 "use strict";
 
-const {nanoid} = require(`nanoid`);
-const {MAX_ID_LENGTH} = require(`../../constants`);
-
 class CommentService {
-  create(comment, article) {
-    const newComment = Object.assign(
-        {id: nanoid(MAX_ID_LENGTH), text: ``},
-        comment
-    );
-
-    article.comments.push(newComment);
-    return newComment;
+  constructor(sequelize) {
+    this._Offer = sequelize.models.Offer;
+    this._Comment = sequelize.models.Comment;
   }
 
-  drop(id, article) {
-    const comments = this.findAll(article);
-    const comment = this.findOne(id, article);
-
-    if (!comment) {
-      return null;
-    }
-
-    article.comments = comments.filter((item) => item.id !== id);
-    return comment;
+  create(articleId, comment) {
+    return this._Comment.create({
+      articleId,
+      ...comment,
+    });
   }
 
-  findAll(article) {
-    return article.comments;
+  async drop(id) {
+    const deletedRows = this._Comment.destroy({
+      where: {id},
+    });
+    return !!deletedRows;
   }
 
-  findOne(id, article) {
-    return article.comments.find((item) => item.id === id);
+  findAll(articleId) {
+    return this._Comment.findAll({
+      where: {articleId},
+      raw: true,
+    });
+  }
+
+  findOne(id) {
+    return this._Comment.findByPk(id);
   }
 }
 
--- src/service/data-service/index.js
+++ src/service/data-service/index.js
@@ -3,9 +3,11 @@
 const CategoryService = require(`./category`);
 const ArticleService = require(`./article`);
 const CommentService = require(`./comment`);
+const SearchService = require(`./search`);
 
 module.exports = {
   CategoryService,
   ArticleService,
   CommentService,
+  SearchService,
 };
